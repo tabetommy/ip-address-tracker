006b988a4d1466dbd2b70ddbbc29b6dd716a494a
Tommy Egbe <tabe_e@yahoo.ca>
2023-01-21 22:44:16 +0100

    added a function to switch betwwen stret map and satelite view

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,32 +1,47 @@
 import React, {useState,useEffect} from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer,
+        TileLayer, 
+        Marker, 
+        Popup,
+        LayersControl  } from 'react-leaflet'
 import L from "leaflet"
 import location from './location.svg'
 import './Map.css'
 
 
 function Map({usercoordinates,dataPoints}){
-   const [coor,setCoor]=useState([])
-   const[loaded,setLoaded]=useState(false)
+   const openStreetMap="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+   const sateliteMap='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'  
+    
+   const [coor,setCoor]=useState([]);
+   const[loaded,setLoaded]=useState(false);
+   
     
     useEffect(()=>{
       if(Object.keys(dataPoints).length === 0)return
         setCoor([dataPoints.latitude,dataPoints.longitude]);
         setLoaded(true)              
-    },[dataPoints])
+    },[dataPoints]);
 
     const markerIcon= new L.Icon({
         iconUrl:location
-    })
-    
+    });
+
     return(
             <MapContainer
-            key={JSON.stringify(coor)}
+                key={JSON.stringify(coor)}
                 center={!loaded?[usercoordinates.latitude,usercoordinates.longitude]:coor}
                  zoom={15} scrollWheelZoom={true} >
-                <TileLayer
-                    url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'  
-                />
+                <LayersControl position="bottomright">
+                    <LayersControl.BaseLayer name="OpenStreetMap View">
+                        <TileLayer 
+                        url={openStreetMap}
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'/>
+                    </LayersControl.BaseLayer>
+                    <LayersControl.BaseLayer name="Satelite View">
+                        <TileLayer url={sateliteMap}/>
+                    </LayersControl.BaseLayer>
+                </LayersControl>
                 <Marker 
                 position={!loaded?[usercoordinates.latitude,usercoordinates.longitude]:coor}
                 icon={markerIcon}
